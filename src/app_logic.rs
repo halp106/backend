07c6aa83f47845577e0cb4e73dd423a935be0b82
Adam Weingram <adamweingram@gmail.com>
2021-12-14 02:29:38 -0800

    Added request guard, auth function, initial DB code

---
+++ src/app_logic.rs
@@ -0,0 +1,238 @@
+use std::path::Path;
+use rusqlite::{params, Connection, Result};
+use chrono::{DateTime, Utc};
+
+#[derive(Debug)]
+struct TestEntry {
+    id: i32,
+    text: String,
+}
+
+pub fn test_db() -> Result<()> {
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute(
+        "CREATE TABLE testing (id INTEGER PRIMARY KEY, text TEXT NOT NULL)",
+        []
+    )?;
+
+    let test_entry = TestEntry {
+        id: 0i32,
+        text: String::from("Testing"),
+    };
+
+    conn.execute(
+        "INSERT INTO testing (id, text) VALUES (?1, ?2)",
+        params![test_entry.id, test_entry.text],
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, text FROM testing")?;
+
+    let row_iter = stmt.query_map([], |row| {
+        Ok(TestEntry {
+            id: row.get(0)?,
+            text: row.get(1)?,
+        })
+    })?;
+
+    for entry in row_iter {
+        println!("Found row: {:?}", entry.unwrap());
+    }
+
+    Ok(())
+}
+
+pub fn connect_db(path: &String, in_memory: bool) -> Result<Connection> {
+    // Get path specified in argument as an actual path
+    let db_path = Path::new(path);
+
+    // Open a connection to the database
+    let conn = match in_memory {
+        true => Connection::open_in_memory()?,
+        false => Connection::open(db_path)?,
+    };
+
+    // Return the connection
+    Ok(conn)
+}
+
+pub fn setup_database(conn: &mut Connection) -> Result<()> {
+
+    // Create Users table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users ( \
+                unique_id INTEGER PRIMARY KEY, \
+                username TEXT UNIQUE NOT NULL, \
+                email TEXT UNIQUE, \
+                password_hash TEXT NOT NULL, \
+                password_salt TEXT NOT NULL, \
+                registration_datetime TEXT \
+            );",
+        []
+    )?;
+
+    // Create Authentication Keys table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS authentication_keys ( \
+                unique_id INTEGER PRIMARY KEY, \
+                user_id INTEGER NOT NULL, \
+                authentication_key TEXT NOT NULL, \
+                expiration TEXT NOT NULL, \
+                FOREIGN KEY (user_id) references users(unique_id) ON DELETE CASCADE \
+            );",
+        []
+    )?;
+
+    // Create the User Privileges table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS user_privileges ( \
+                privilege_unique_id INTEGER PRIMARY KEY, \
+                user_id INTEGER NOT NULL, \
+                privilege TEXT NOT NULL, \
+                FOREIGN KEY (user_id) references users(unique_id) ON DELETE CASCADE \
+            );",
+        []
+    )?;
+
+    // Create the Threads/Posts table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS threads ( \
+                unique_id INTEGER PRIMARY KEY, \
+                title TEXT NOT NULL, \
+                creator_uid INTEGER NOT NULL, \
+                creation_timestamp TEXT, \
+                tag TEXT, \
+                content TEXT NOT NULL, \
+                FOREIGN KEY (creator_uid) references users(unique_id) ON DELETE CASCADE \
+            );",
+        []
+    )?;
+
+    // Create Comments table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS comments ( \
+                unique_id INTEGER PRIMARY KEY, \
+                thread_id INTEGER NOT NULL, \
+                creator_uid INTEGER NOT NULL, \
+                creation_timestamp TEXT, \
+                content TEXT NOT NULL, \
+                FOREIGN KEY (thread_id) references threads(unique_id) ON DELETE CASCADE, \
+                FOREIGN KEY (creator_uid) references users(unique_id) ON DELETE CASCADE \
+            );",
+        []
+    )?;
+
+    // Create the "root" admin user
+    // todo!("Add correct SQL to create the 'root' admin user");
+    // conn.execute(
+    //     "INSERT INTO users ( \
+    //             username, \
+    //             email, \
+    //             password_hash, \
+    //             password_salt, \
+    //             registration_datetime) \
+    //          VALUES (1?, 2?, 3?, 4?, 5?)",
+    //     params!["..."]
+    // )?;
+    //
+    // conn.execute(
+    //     "INSERT INTO user_privileges (user_id, privilege) VALUES (?1, ?2)",
+    //     params!["..."]
+    // )?;
+
+    // Return success if everything completes
+    Ok(())
+}
+
+pub fn authenticate(conn: &mut Connection, auth_key: &String) -> Result<bool> {
+    // Verifies a username and authentication token against the database (and expiration datetime)
+
+    // Get the current time
+    let now = Utc::now();
+    println!("Time now is: {}", now.to_rfc3339());
+
+    // Prepare query in the DB that retrieves all matching authentication keys
+    let mut auth_keys_query = match conn.prepare("SELECT \
+                        expiration \
+                      FROM \
+                        authentication_keys \
+                      WHERE \
+                        authentication_key = ?1"
+    ) {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Error encountered while running query for Authenticate: {}", e);
+            panic!("Panicked after not being able to resolve error!")
+        }
+    };
+
+    // Iterate through results and verify that the authentication key has not expired
+    // Note: Also checks for lack of auth tokens by never running expiration checks if no matching
+    // rows exist
+    let mut valid_key_encountered = false;
+    let auth_key_results = auth_keys_query.query_map(params![auth_key], |row| {
+        // Get values from the row as String objects
+        let expiration: String = row.get(1)?;
+
+        // Parse the row values
+        let expiration_datetime = match DateTime::parse_from_rfc3339(expiration.as_str()) {
+            Ok(val) => val,
+            Err(e) => {
+                println!("Error when parsing expiration datetime: {}", e);
+                panic!("Panicked while parsing expiration datetime!");
+            }
+        };
+
+        // Check expiration datetime against current time, set flag if still valid
+        let duration_since_expiration = now.signed_duration_since(expiration_datetime);
+        let mut not_expired_flag = false;
+        if duration_since_expiration.num_seconds() < 0 {
+            valid_key_encountered = true;
+            not_expired_flag = true;
+        }
+
+        Ok(not_expired_flag)
+    })?;
+
+    // Actually run iterator (and count how many expired authentication keys have been collected)
+    let mut expired_tokens = 0;
+    for entry in auth_key_results {
+        let safe_entry = entry?;
+        if safe_entry {
+            expired_tokens += 1;
+        }
+    }
+    println!("Found {} expired token(s) while authenticating!", expired_tokens);
+
+    // Return the result of the expiration check
+    Ok(valid_key_encountered)
+}
+
+pub fn login(conn: &mut Connection, username: &String, password: &String) -> Result<String> {
+    // Creates an authentication token for a user given the user's password
+    todo!("Implement login function")
+}
+
+pub fn get_posts(conn: &mut Connection, ) -> Result<String> {
+    todo!("Implement get_posts function")
+}
+
+pub fn get_post_comments(conn: &mut Connection, thread_uid: &String) -> Result<String> {
+    todo!("Implement get_post_comments function")
+}
+
+pub fn create_post(conn: &mut Connection, title: &String, username: &String, timestamp: &String, tag: &String, content: &String) -> Result<bool> {
+    todo!("Implement create_post function")
+}
+
+pub fn create_comment(conn: &mut Connection, thread_uid: &String, username: &String, timestamp: &String, content: &String) -> Result<bool> {
+    todo!("Implement create_comment function")
+}
+
+pub fn delete_thread(conn: &mut Connection, thread_uid: &String) -> Result<bool> {
+    todo!("Implement delete_thread function")
+}
+
+pub fn delete_comment(conn: &mut Connection, thread_uid: &String) -> Result<bool> {
+    todo!("Implement delete_comment function")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+mod app_logic;
+
 #[macro_use] extern crate rocket;
 
 use rocket::http::Header;
-use rocket::{Build, Data, Orbit, Request, Response, Rocket};
+use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
+use rocket::request::{FromRequest, Outcome};
+use rocket::serde::{Serialize, Deserialize, json::Json};
+use rocket::serde::json::serde_json::json;
+use rocket::http::Status;
+use rusqlite::Connection;
 
 // Set up CORS
 pub struct CORS;
@@ -26,20 +33,119 @@ impl Fairing for CORS {
     }
 }
 
+// Data Structs
+struct DbState {
+    in_memory: bool,
+    db_path: String,
+}
+
+#[derive(Deserialize)]
+struct LoginInfo<'r> {
+    username: &'r str,
+    password: &'r str,
+}
+
+#[derive(Serialize)]
+struct Post<'r> {
+    unique_id: &'r str,
+    title: &'r str,
+    author_username: &'r str,
+    timestamp: &'r str,
+    tag: &'r str,
+    content: &'r str,
+}
+
+// Request Guards
+pub struct AuthenticationKey {
+    key_content: String,
+}
+
+#[derive(Debug)]
+pub enum AuthenticationKeyError {
+    Missing,
+    Invalid,
+    DbError
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthenticationKey {
+    type Error = AuthenticationKeyError;
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        // Retrieve DB managed state
+        let db_state = match request.rocket().state::<DbState>() {
+            Some(res) => res,
+            None => return Outcome::Failure((Status::InternalServerError, AuthenticationKeyError::DbError)),
+        };
+
+        // Get the authentication key from the request header
+        let auth_key = match request.headers().get_one("x-auth-key") {
+            Some(res) => res.to_string(),
+            None => return Outcome::Failure((Status::Unauthorized, AuthenticationKeyError::Missing)),
+        };
+
+        // Get a connection to the database
+        let mut conn = match app_logic::connect_db(&db_state.db_path, db_state.in_memory) {
+            Ok(res) => res,
+            Err(_) => return Outcome::Failure((Status::InternalServerError, AuthenticationKeyError::DbError)),
+        };
+
+        // Check the authentication key against the database and return
+        match app_logic::authenticate(&mut conn, &auth_key) {
+            Ok(res) => match res {
+                true => Outcome::Success(AuthenticationKey {
+                    key_content: auth_key
+                }),
+                false => Outcome::Failure((Status::Unauthorized, AuthenticationKeyError::Invalid)),
+            },
+            Err(_) => Outcome::Failure((Status::InternalServerError, AuthenticationKeyError::DbError)),
+        }
+    }
+}
+
 // Routing & Handlers
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[post("/login")]
-fn login() -> &'static str {
+#[post("/login", data="<input>")]
+fn login(input: Json<LoginInfo<'_>>) -> rocket::serde::json::Value {
+    // [DEBUG] Print out the username and password received from the user
+    println!("Username: {}, Password: {}", input.username, input.password);
 
-    "Login attempt"
+    // [TODO] Call authentication function in login request handler
+
+    // Return the authentication key for this user
+    json!({
+        "auth_key": "IMPLEMENT_ME!",
+        "expiration_datetime": "Never?"
+    })
+}
+
+#[get("/posts")]
+fn get_posts(authentication_key: AuthenticationKey) -> &'static str {
+    let _ = app_logic::test_db();
+
+    "Implement me!"
 }
 
 // Launch
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(CORS).mount("/", routes![index, login])
+    // Init variables
+    let db_state = DbState {
+        in_memory: false,
+        db_path: String::from("./test_db.sqlite"),
+    };
+
+    // Run initial setup & get DB connection
+    let mut db_conn = app_logic::connect_db(&db_state.db_path, db_state.in_memory).unwrap();
+    app_logic::setup_database(&mut db_conn).unwrap();
+
+    // Run Rocket setup
+    rocket::build()
+        .manage(db_state)  // Manage DB state
+        .attach(CORS)
+        .mount("/", routes![index, login, get_posts])
 }
